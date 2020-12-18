e2ae1e84ee2f3538b1483492c48be17bc59c6153
Junrui Liu <andyliu0248@gmail.com>
2020-12-18 17:13:25 -0500

    Parse z3 model

--- src/see.py
+++ src/see.py
@@ -3,6 +3,8 @@
 import argparse
 import subprocess as sp
 
+import sexpdata as sexp
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=argparse.FileType('r'), help="program source file")
@@ -28,22 +30,35 @@ if __name__ == "__main__":
   try:
     cmd = f"cabal -v0 run see-hs {args.file.name} {args.n}"
     res = sp.run(cmd, shell=True, capture_output=True, check=True)
-    formula = get_stdout(res)
+    out = get_stdout(res)
+    program_name, *formulas = out.split("\n;SEP\n")
     
     if args.dry_run: # dry run
-      print(formula)
+      print(formulas)
     
     else:
-      print(formula)
-      # TODO for loop on args.n
-      cmd = f"echo \"{formula}\" | z3 -in"
-      res = sp.run(cmd, shell=True, capture_output=True, check=True)
+      models = list()
+      for formula in formulas:
+        cmd = f"echo \"{formula}\" | z3 -in"
+        res = sp.run(cmd, shell=True, capture_output=True, check=True)
+        out = [l.strip() for l in get_stdout(res).split('\n')]
+
+        if "sat" in out:
+          model_str = "".join(out[1:])
+          model = [pair[1] for pair in sexp.loads(model_str)]
+          models.append(model)
+        elif "unsat" in out:
+          pass
+        else:
+          raise ValueError("Output is neither sat nor unsat")
       
-      if get_stdout(res) == "sat":
-        print(INVALID_MSG)
-        # TODO parse SAT assignment
-      elif get_stdout(res) == "unsat":
+      if len(models) == 0:
         print(VALID_MSG)
+
+      else:
+        print(INVALID_MSG)
+        for model in models:
+          print(program_name.strip(), " ".join(map(str, model)))
 
   except sp.CalledProcessError as err:
     print(ERROR_MSG, get_stderr(err))
