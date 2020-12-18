813030c92952c42fdb056720ec52e984ece26d1a
Junrui Liu <andyliu0248@gmail.com>
2020-12-17 19:06:42 -0500

    Compile with `-Wall` flag

--- makefile
+++ makefile
@@ -1,10 +1,12 @@
 export PATH := $(shell pwd)/bin:$(PATH)
 
 see:
-	cabal build :see-hs
-	mkdir -p bin
-	echo "#!/bin/bash\npython3 src/see.py" '"$$@"' > bin/see
-	chmod +x bin/see
+	cabal configure \
+	&& cabal build --ghc-options="-Wall" \
+	&& mkdir -p bin \
+	&& echo "#!/bin/bash\npython3 src/see.py" '"$$@"' > bin/see \
+	&& chmod +x bin/see
 
 clean:
+	cabal clean
 	rm -rf bin dist-newstyle
--- src/see.py
+++ src/see.py
@@ -3,6 +3,7 @@
 import argparse
 import subprocess as sp
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=argparse.FileType('r'), help="program source file")
 parser.add_argument("n", type=int, help="maximum number of times loops are unrolled")
@@ -13,22 +14,28 @@ VALID_MSG = "Verified"
 INVALID_MSG = "Not verified"
 ERROR_MSG = "Error"
 
+
 def get_stdout(res):
   return res.stdout.decode('utf-8').strip()
 
 def get_stderr(res):
   return res.stderr.decode('utf-8').strip()
 
+
 if __name__ == "__main__":
   args = parser.parse_args()
+  
   try:
     cmd = f"cabal -v0 run see-hs {args.file.name} {args.n}"
     res = sp.run(cmd, shell=True, capture_output=True, check=True)
+    
     if args.dry_run: # dry run
       print(get_stdout(res))
+    
     else:
       cmd = f"echo \"{res}\" | z3 -in"
       res = sp.run(cmd, shell=True, capture_output=True, check=True)
+      
       if get_stdout(res) == "sat":
         print(VALID_MSG)
       else:
